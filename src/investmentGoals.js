8e93dcbfbedf88910ac6691e8f7d4ac9de5fef23
Renan Araujo <renan290812@gmail.com>
2024-05-27 08:01:48 -0300

    formulario de investimento e arquivo e cálculo de projeções

---
+++ src/investmentGoals.js
@@ -0,0 +1,57 @@
+function convertToMonthlyReturnRate(yearlyreturnRate) {
+  return yearlyreturnRate ** (1 / 12);
+}
+
+function generateReturnsArray(
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = 'monthly',
+  monthlyContribution = 0,
+  returnRate = 0,
+  returnTimeFrame = 'monthly'
+) {
+  if (!timeHorizon || !startingAmount) {
+    throw new Error(
+      'Investimento inicial e prazo devem ser preenchidos com valores positivos'
+    );
+  }
+  const finalReturnRate =
+    returnTimeFrame === 'monthly'
+      ? 1 + returnRate / 100
+      : convertToMonthlyReturnRate(1 + returnRate / 100);
+
+  const finalTimeHorizon =
+    timePeriod === 'monthly' ? timeHorizon : timeHorizon * 12;
+
+  const referenceInvestmentObject = {
+    investedAmount: startingAmount,
+    interestReturns: 0,
+    totalInterestReturns: 0,
+    month: 0,
+    totalAmount: startingAmount,
+  };
+
+  const returnArrays = [referenceInvestmentObject];
+  for (
+    let timeReference = 1;
+    timeReference <= finalTimeHorizon;
+    timeReference++
+  ) {
+    const totalAmount =
+      returnArrays[timeReference - 1].totalAmount * finalReturnRate +
+      monthlyContribution;
+    const interestReturns =
+      returnArrays[timeReference - 1].totalAmount * finalReturnRate;
+    const investedAmount = startingAmount + monthlyContribution * timeReference;
+    const totalInterestReturns = totalAmount - investedAmount;
+    returnArrays.push({
+      investedAmount: investedAmountartingAmount,
+      interestReturns: interestReturns,
+      totalInterestReturns: totalInterestReturns,
+      month: timeReference,
+      totalAmount: totalAmount,
+    });
+  }
+  return returnArrays
+}
+ 
